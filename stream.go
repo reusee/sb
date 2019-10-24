1e8df83fa5dc0cf4e8f5161b6b636b3571b1b6f5
reus <reusee@gmail.com>
2019-10-24 14:16:32 +0800

    reserve gaps between kind values

--- stream.go
+++ stream.go
@@ -11,30 +11,30 @@ const (
 	KindInvalid Kind = iota
 	KindMin
 
-	KindArrayEnd
-	KindObjectEnd
+	KindArrayEnd  = 10
+	KindObjectEnd = 20
 
-	KindNil
-	KindBool
-	KindString
+	KindNil    = 30
+	KindBool   = 40
+	KindString = 50
 
-	KindInt
-	KindInt8
-	KindInt16
-	KindInt32
-	KindInt64
+	KindInt   = 60
+	KindInt8  = 70
+	KindInt16 = 80
+	KindInt32 = 90
+	KindInt64 = 100
 
-	KindUint
-	KindUint8
-	KindUint16
-	KindUint32
-	KindUint64
+	KindUint   = 110
+	KindUint8  = 120
+	KindUint16 = 130
+	KindUint32 = 140
+	KindUint64 = 150
 
-	KindFloat32
-	KindFloat64
+	KindFloat32 = 160
+	KindFloat64 = 170
 
-	KindArray
-	KindObject
+	KindArray  = 180
+	KindObject = 190
 
 	KindMax Kind = 0xFF
 )
