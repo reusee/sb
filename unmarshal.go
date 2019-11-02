d912fb58fd4a85924321257754100200803dcdba
reus <reusee@gmail.com>
2019-11-02 13:18:37 +0800

    simplify unmarshal of pointer

--- unmarshal.go
+++ unmarshal.go
@@ -84,15 +84,10 @@ func UnmarshalValue(stream Stream, ptr reflect.Value) error {
 	hasConcreteType := false
 	if valueKind == reflect.Ptr {
 		// deref
-		valuePtr := ptr.Elem()
-		if valuePtr.IsNil() {
-			valuePtr = reflect.New(valueType.Elem())
+		if ptr.Elem().IsNil() {
+			ptr.Elem().Set(reflect.New(valueType.Elem()))
 		}
-		if err := UnmarshalValue(stream, valuePtr); err != nil {
-			return err
-		}
-		ptr.Elem().Set(valuePtr)
-		return nil
+		return UnmarshalValue(stream, ptr.Elem())
 	} else if valueKind != reflect.Interface {
 		hasConcreteType = true
 		if ptr.IsNil() {
