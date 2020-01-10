81d0ded315972b0150dede50d64d3ac7f8ea70c7
reus <reusee@gmail.com>
2020-01-10 18:47:28 +0800

    add Tree.IterFunc

--- tree_iter.go
+++ tree_iter.go
@@ -47,3 +47,63 @@ func IterSubTrees(
 		), nil
 	}
 }
+
+func (t *Tree) IterFunc(
+	fn func(*Tree) (*Token, error),
+) *Proc {
+	proc := IterTreeFunc(t, fn, nil)
+	return &proc
+}
+
+func IterTreeFunc(
+	tree *Tree,
+	fn func(*Tree) (*Token, error),
+	cont Proc,
+) Proc {
+	return func() (*Token, Proc, error) {
+		token, err := fn(tree)
+		if err != nil {
+			return nil, nil, err
+		}
+		if token != nil {
+			return token, cont, nil
+		}
+		return tree.Token, IterSubTreesFunc(
+			tree.Subs, 0, fn,
+			func() (*Token, Proc, error) {
+				if len(tree.Hash) > 0 {
+					return &Token{
+						Kind:  KindPostHash,
+						Value: tree.Hash,
+					}, cont, nil
+				}
+				return nil, cont, nil
+			},
+		), nil
+	}
+}
+
+func IterSubTreesFunc(
+	subs []*Tree,
+	index int,
+	fn func(*Tree) (*Token, error),
+	cont Proc,
+) Proc {
+	return func() (*Token, Proc, error) {
+		if len(subs) == 0 {
+			return nil, cont, nil
+		}
+		if index >= len(subs) {
+			return nil, cont, nil
+		}
+		return nil, IterTreeFunc(
+			subs[index],
+			fn,
+			IterSubTrees(
+				subs,
+				index+1,
+				cont,
+			),
+		), nil
+	}
+}
