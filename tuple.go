861d2b6a1b3340ad27d044046fd708eafecabe4b
reus <reusee@gmail.com>
2020-02-28 10:54:02 +0800

    add UnmarshalTupleTyped

--- tuple.go
+++ tuple.go
@@ -1,6 +1,7 @@
 package sb
 
 import (
+	"fmt"
 	"reflect"
 )
 
@@ -36,4 +37,66 @@ func marshalTuple(vm ValueMarshalFunc, tuple Tuple, cont Proc) Proc {
 			cont,
 		),
 	)
+}
+
+func UnmarshalTupleTyped(typeSpec any, target *Tuple, cont Sink) Sink {
+	return func(token *Token) (Sink, error) {
+		if token == nil {
+			return nil, UnmarshalError{ExpectingTuple}
+		}
+		if token.Kind != KindTuple {
+			return nil, UnmarshalError{ExpectingTuple}
+		}
+
+		var types []reflect.Type
+		spec := reflect.ValueOf(typeSpec)
+		specType := spec.Type()
+		switch specType.Kind() {
+		case reflect.Func:
+			for i := 0; i < specType.NumIn(); i++ {
+				types = append(types, specType.In(i))
+			}
+		case reflect.Struct:
+			for i := 0; i < specType.NumField(); i++ {
+				types = append(types, specType.Field(i).Type)
+			}
+		default: // NOCOVER
+			panic(fmt.Errorf("bad type: %T", typeSpec))
+		}
+
+		return unmarshalTupleTyped(types, target, cont), nil
+	}
+}
+
+func unmarshalTupleTyped(types []reflect.Type, target *Tuple, cont Sink) Sink {
+	return func(token *Token) (Sink, error) {
+		if token == nil {
+			return nil, UnmarshalError{ExpectingValue}
+		}
+
+		if token.Kind == KindTupleEnd {
+			if len(types) > 0 {
+				return nil, UnmarshalError{ExpectingValue}
+			}
+			return cont, nil
+		}
+
+		if len(types) == 0 {
+			return nil, UnmarshalError{TooManyElement}
+		}
+
+		elem := reflect.New(types[0])
+		return UnmarshalValue(
+			elem,
+			func(token *Token) (Sink, error) {
+				*target = append(*target, elem.Elem().Interface())
+				return unmarshalTupleTyped(
+					types[1:],
+					target,
+					cont,
+				)(token)
+			},
+		)(token)
+
+	}
 }
--- tuple_test.go
+++ tuple_test.go
@@ -20,3 +20,95 @@ func TestTuple(t *testing.T) {
 		t.Fatal(err)
 	}
 }
+
+func TestTupleUnmarshalTyped(t *testing.T) {
+
+	var tuple Tuple
+	if err := Pipe(
+		NewMarshaler(Tuple{
+			42, true, "foo",
+		}),
+		UnmarshalTupleTyped(func(int, bool, string) {}, &tuple, nil),
+	); err != nil {
+		t.Fatal(err)
+	}
+	if tuple[0] != 42 {
+		t.Fatal()
+	}
+	if tuple[1] != true {
+		t.Fatal()
+	}
+	if tuple[2] != "foo" {
+		t.Fatal()
+	}
+
+	if err := Pipe(
+		NewMarshaler(Tuple{
+			1, false, "bar",
+		}),
+		UnmarshalTupleTyped(struct {
+			int
+			bool
+			string
+		}{}, &tuple, nil),
+	); err != nil {
+		t.Fatal(err)
+	}
+	if tuple[0] != 42 {
+		t.Fatal()
+	}
+	if tuple[1] != true {
+		t.Fatal()
+	}
+	if tuple[2] != "foo" {
+		t.Fatal()
+	}
+
+	if err := Pipe(
+		Tokens{}.Iter(),
+		UnmarshalTupleTyped(func(int, bool, string) {}, &tuple, nil),
+	); !is(err, ExpectingTuple) {
+		t.Fatal(err)
+	}
+
+	if err := Pipe(
+		Tokens{
+			Token{
+				Kind: KindString,
+			},
+		}.Iter(),
+		UnmarshalTupleTyped(func(int, bool, string) {}, &tuple, nil),
+	); !is(err, ExpectingTuple) {
+		t.Fatal(err)
+	}
+
+	if err := Pipe(
+		Tokens{
+			Token{
+				Kind: KindTuple,
+			},
+		}.Iter(),
+		UnmarshalTupleTyped(func(int, bool, string) {}, &tuple, nil),
+	); !is(err, ExpectingValue) {
+		t.Fatal(err)
+	}
+
+	if err := Pipe(
+		NewMarshaler(Tuple{
+			42, true, "foo",
+		}),
+		UnmarshalTupleTyped(func(int, bool) {}, &tuple, nil),
+	); !is(err, TooManyElement) {
+		t.Fatal(err)
+	}
+
+	if err := Pipe(
+		NewMarshaler(Tuple{
+			42, true,
+		}),
+		UnmarshalTupleTyped(func(int, bool, string) {}, &tuple, nil),
+	); !is(err, ExpectingValue) {
+		t.Fatal(err)
+	}
+
+}
