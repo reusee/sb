a8209dc74cb10266923fa1db5a7d8723d824db0c
reus <reusee@gmail.com>
2020-02-29 22:57:06 +0800

    add Encoder

--- encoder.go
+++ encoder.go
@@ -6,18 +6,18 @@ import (
 	"io"
 )
 
-func Encode(w io.Writer, stream Stream) error {
+func Encoder(w io.Writer, cont Sink) Sink {
 	buf := make([]byte, 8)
-	for {
-		token, err := stream.Next()
-		if err != nil {
-			return err
-		}
+	return encoder(w, buf, cont)
+}
+
+func encoder(w io.Writer, buf []byte, cont Sink) Sink {
+	return func(token *Token) (Sink, error) {
 		if token == nil {
-			break
+			return cont, nil
 		}
 		if err := binary.Write(w, binary.LittleEndian, token.Kind); err != nil {
-			return err
+			return nil, err
 		}
 		if token.Value != nil {
 			switch value := token.Value.(type) {
@@ -25,67 +25,67 @@ func Encode(w io.Writer, stream Stream) error {
 			case bool:
 				if value {
 					if _, err := w.Write([]byte{1}); err != nil { // NOCOVER
-						return err
+						return nil, err
 					}
 				} else {
 					if _, err := w.Write([]byte{0}); err != nil { // NOCOVER
-						return err
+						return nil, err
 					}
 				}
 
 			case int:
 				if err := binary.Write(w, binary.LittleEndian, int64(value)); err != nil { // NOCOVER
-					return err
+					return nil, err
 				}
 
 			case uint:
 				if err := binary.Write(w, binary.LittleEndian, uint64(value)); err != nil { // NOCOVER
-					return err
+					return nil, err
 				}
 
 			case int8, int16, int32, int64,
 				uint8, uint16, uint32, uint64,
 				float32, float64:
 				if err := binary.Write(w, binary.LittleEndian, value); err != nil { // NOCOVER
-					return err
+					return nil, err
 				}
 
 			case string:
 				l := uint64(len(value))
 				if l < 128 {
 					if _, err := w.Write([]byte{byte(l)}); err != nil { // NOCOVER
-						return err
+						return nil, err
 					}
 				} else {
 					n := binary.PutUvarint(buf, l)
 					if _, err := w.Write([]byte{byte(^n)}); err != nil { // NOCOVER
-						return err
+						return nil, err
 					}
 					if _, err := w.Write(buf[:n]); err != nil { // NOCOVER
-						return err
+						return nil, err
 					}
 				}
 				if _, err := w.Write([]byte(value)); err != nil { // NOCOVER
-					return err
+					return nil, err
 				}
 
 			case []byte:
 				l := uint64(len(value))
 				if l < 128 {
 					if _, err := w.Write([]byte{byte(l)}); err != nil { // NOCOVER
-						return err
+						return nil, err
 					}
 				} else {
 					n := binary.PutUvarint(buf, l)
 					if _, err := w.Write([]byte{byte(^n)}); err != nil { // NOCOVER
-						return err
+						return nil, err
 					}
 					if _, err := w.Write(buf[:n]); err != nil { // NOCOVER
-						return err
+						return nil, err
 					}
 				}
 				if _, err := w.Write(value); err != nil { // NOCOVER
-					return err
+					return nil, err
 				}
 
 			default: // NOCOVER
@@ -93,7 +93,11 @@ func Encode(w io.Writer, stream Stream) error {
 
 			}
 		}
-	}
 
-	return nil
+		return encoder(w, buf, cont), nil
+	}
+}
+
+func Encode(w io.Writer, stream Stream) error {
+	return Pipe(stream, Encoder(w, nil))
 }
