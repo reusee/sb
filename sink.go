823217878dbc22c09b4fc7d4024cf001760ea7d5
reus <reusee@gmail.com>
2020-03-01 08:59:13 +0800

    clean-ups

--- sink.go
+++ sink.go
@@ -25,3 +25,61 @@ func AltSink(sinks ...Sink) Sink {
 		return AltSink(next...), nil
 	}
 }
+
+func Pipe(stream Stream, sinks ...Sink) error {
+	var err error
+	for {
+		token, err := stream.Next()
+		if err != nil {
+			return err
+		}
+		if token == nil {
+			break
+		}
+		if len(sinks) == 0 {
+			return UnmarshalError{EmptySink}
+		}
+		for i := 0; i < len(sinks); {
+			if sinks[i] == nil {
+				sinks[i] = sinks[len(sinks)-1]
+				sinks = sinks[:len(sinks)-1]
+				continue
+			}
+			sinks[i], err = sinks[i](token)
+			if err != nil {
+				return err
+			}
+			i++
+		}
+	}
+
+	for len(sinks) > 0 {
+		for i := 0; i < len(sinks); {
+			if sinks[i] == nil {
+				sinks[i] = sinks[len(sinks)-1]
+				sinks = sinks[:len(sinks)-1]
+				continue
+			}
+			sinks[i], err = sinks[i](nil)
+			if err != nil {
+				return err
+			}
+			i++
+		}
+	}
+
+	return nil
+}
+
+func ExpectKind(kind Kind, cont Sink) Sink {
+	return func(token *Token) (Sink, error) {
+		if token == nil || token.Kind != kind {
+			if err, ok := kindToExpectingErr[kind]; ok {
+				return nil, UnmarshalError{err}
+			} else {
+				return nil, UnmarshalError{ExpectingValue}
+			}
+		}
+		return cont, nil
+	}
+}
--- unmarshal.go
+++ unmarshal.go
@@ -26,64 +26,6 @@ func Unmarshal(stream Stream, targets ...any) error {
 	return Pipe(stream, sinks...)
 }
 
-func Pipe(stream Stream, sinks ...Sink) error {
-	var err error
-	for {
-		token, err := stream.Next()
-		if err != nil {
-			return err
-		}
-		if token == nil {
-			break
-		}
-		if len(sinks) == 0 {
-			return UnmarshalError{EmptySink}
-		}
-		for i := 0; i < len(sinks); {
-			if sinks[i] == nil {
-				sinks[i] = sinks[len(sinks)-1]
-				sinks = sinks[:len(sinks)-1]
-				continue
-			}
-			sinks[i], err = sinks[i](token)
-			if err != nil {
-				return err
-			}
-			i++
-		}
-	}
-
-	for len(sinks) > 0 {
-		for i := 0; i < len(sinks); {
-			if sinks[i] == nil {
-				sinks[i] = sinks[len(sinks)-1]
-				sinks = sinks[:len(sinks)-1]
-				continue
-			}
-			sinks[i], err = sinks[i](nil)
-			if err != nil {
-				return err
-			}
-			i++
-		}
-	}
-
-	return nil
-}
-
-func ExpectKind(kind Kind, cont Sink) Sink {
-	return func(token *Token) (Sink, error) {
-		if token == nil || token.Kind != kind {
-			if err, ok := kindToExpectingErr[kind]; ok {
-				return nil, UnmarshalError{err}
-			} else {
-				return nil, UnmarshalError{ExpectingValue}
-			}
-		}
-		return cont, nil
-	}
-}
-
 func UnmarshalValue(target reflect.Value, cont Sink) Sink {
 
 	if target.IsValid() {
