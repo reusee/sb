bd6ab1959e5d899a1ac5ed7b4873c3185e446528
reus <reusee@gmail.com>
2020-03-17 08:31:04 +0800

    fix toBytes test

--- bytes_test.go
+++ bytes_test.go
@@ -1,10 +1,16 @@
 package sb
 
 import (
+	"bytes"
 	"reflect"
 	"testing"
 )
 
 func TestToBytes(t *testing.T) {
-	toBytes(reflect.ValueOf([3]byte{1}))
+	if !bytes.Equal(
+		toBytes(reflect.ValueOf([3]byte{1, 1, 1})),
+		[]byte{1, 1, 1},
+	) {
+		t.Fatal()
+	}
 }
