4e20882289f4d0b2194188a7e6098cb2fd43cc64
reus <reusee@gmail.com>
2020-03-25 16:55:16 +0800

    simplify Proc.Next

--- proc.go
+++ proc.go
@@ -4,18 +4,15 @@ type Proc func() (*Token, Proc, error)
 
 var _ Stream = (*Proc)(nil)
 
-func (p *Proc) Next() (*Token, error) {
-	for {
-		if *p == nil {
-			return nil, nil
-		}
-		var ret *Token
-		var err error
-		ret, *p, err = (*p)()
-		if ret != nil || err != nil {
-			return ret, err
+func (p *Proc) Next() (ret *Token, err error) {
+	for ret == nil {
+		if p != nil && *p != nil {
+			ret, *p, err = (*p)()
+		} else {
+			break
 		}
 	}
+	return
 }
 
 var _ SBMarshaler = Proc(nil)
