71d5fbdf643f5a337eaade2ecbdb3ec29e68d556
reus <reusee@gmail.com>
2021-03-18 21:31:41 +0800

    add ConcatStream, ConcatSink

---
+++ concat.go
@@ -0,0 +1,57 @@
+package sb
+
+func ConcatSinks(sinks ...Sink) Sink {
+	if len(sinks) == 0 {
+		return nil
+	}
+	for sinks[0] == nil {
+		sinks = sinks[1:]
+		if len(sinks) == 0 {
+			return nil
+		}
+	}
+	var sink Sink
+	sink = func(token *Token) (Sink, error) {
+		var err error
+		sinks[0], err = sinks[0].Sink(token)
+		if err != nil {
+			return nil, err
+		}
+		for sinks[0] == nil {
+			sinks = sinks[1:]
+			if len(sinks) == 0 {
+				return nil, nil
+			}
+		}
+		return sink, nil
+	}
+	return sink
+}
+
+func ConcatStream(streams ...Stream) Stream {
+	if len(streams) == 0 {
+		return nil
+	}
+	for streams[0] == nil {
+		streams = streams[1:]
+		if len(streams) == 0 {
+			return nil
+		}
+	}
+	var proc Proc
+	proc = func() (*Token, Proc, error) {
+		token, err := streams[0].Next()
+		if err != nil {
+			return nil, nil, err
+		}
+		if token == nil {
+			streams = streams[1:]
+			if len(streams) == 0 {
+				return nil, nil, nil
+			}
+			return nil, proc, nil
+		}
+		return token, proc, nil
+	}
+	return &proc
+}
---
+++ concat_test.go
@@ -0,0 +1,61 @@
+package sb
+
+import (
+	"testing"
+)
+
+func TestConcat(t *testing.T) {
+	var a any
+	var b any
+	if err := Copy(
+		ConcatStream(
+			Marshal(42),
+			Marshal(2),
+		),
+		ConcatSinks(
+			Unmarshal(&a),
+			Unmarshal(&b),
+		),
+	); err != nil {
+		t.Fatal(err)
+	}
+	if a != 42 {
+		t.Fatal()
+	}
+	if b != 2 {
+		t.Fatal()
+	}
+}
+
+func TestConcatNilSinks(t *testing.T) {
+	if err := Copy(
+		Marshal(42),
+		ConcatSinks(),
+	); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := Copy(
+		Marshal(42),
+		ConcatSinks(nil),
+	); err != nil {
+		t.Fatal(err)
+	}
+
+	var i int
+	if err := Copy(
+		Marshal(42),
+		ConcatSinks(
+			nil,
+			nil,
+			Unmarshal(&i),
+			nil,
+			nil,
+		),
+	); err != nil {
+		t.Fatal(err)
+	}
+	if i != 42 {
+		t.Fatal()
+	}
+}
