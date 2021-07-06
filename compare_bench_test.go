6b1b98994c82072681e3218737c2746a64573510
reus <reusee@gmail.com>
2021-07-07 07:57:56 +0800

    add BenchmarkCompareBytes

--- compare_bench_test.go
+++ compare_bench_test.go
@@ -38,3 +38,37 @@ func BenchmarkCompare(b *testing.B) {
 	}
 
 }
+
+func BenchmarkCompareBytes(b *testing.B) {
+	buf := new(bytes.Buffer)
+	if err := Copy(
+		Marshal(func() string {
+			return "foo"
+		}),
+		Encode(buf),
+	); err != nil {
+		b.Fatal(err)
+	}
+	bs1 := buf.Bytes()
+
+	buf = new(bytes.Buffer)
+	if err := Copy(
+		Marshal(func() string {
+			return "FOOBAR"
+		}),
+		Encode(buf),
+	); err != nil {
+		b.Fatal(err)
+	}
+	bs2 := buf.Bytes()
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		MustCompareBytes(
+			bs1,
+			bs2,
+		)
+	}
+
+}
