248b0e9cb36e1b2828815f406e76e92d671570c7
reus <reusee@gmail.com>
2021-11-06 23:27:37 +0800

    add go fuzzing function

--- fuzz_test.go
+++ fuzz_test.go
@@ -37,3 +37,44 @@ func TestFuzzCorpus(t *testing.T) {
 		Fuzz(data)
 	}
 }
+
+func FuzzAll(f *testing.F) {
+
+	files, err := filepath.Glob("corpus/*")
+	if err != nil {
+		panic(err)
+	}
+	for _, file := range files {
+		data, err := ioutil.ReadFile(file)
+		if err != nil {
+			panic(err)
+		}
+		f.Add(data)
+	}
+
+	files, err = filepath.Glob("crashers/*")
+	if err != nil {
+		panic(err)
+	}
+	for _, file := range files {
+		if strings.HasSuffix(file, ".output") {
+			continue
+		}
+		if strings.HasSuffix(file, ".quoted") {
+			continue
+		}
+		data, err := ioutil.ReadFile(file)
+		if err != nil {
+			panic(err)
+		}
+		f.Add(data)
+	}
+
+	f.Fuzz(func(t *testing.T, data []byte) {
+		switch Fuzz(data) {
+		case 0:
+			t.Skip()
+		}
+	})
+
+}
