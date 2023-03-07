712e1e00a7cf46c468d3e52ef32b76e6638bc826
reus <reusee@gmail.com>
2023-03-08 00:16:35 +0800

    update pr2

--- go.mod
+++ go.mod
@@ -4,5 +4,5 @@ go 1.18
 
 require (
 	github.com/reusee/e5 v0.0.0-20221121150545-535ad462f830
-	github.com/reusee/pr2 v0.0.0-20230221022320-e074b9be574d
+	github.com/reusee/pr2 v0.0.0-20230306155640-52a016ca8efe
 )
--- pool.go
+++ pool.go
@@ -3,13 +3,13 @@ package sb
 import "github.com/reusee/pr2"
 
 // 8 * 1024 = 8K
-var bytesPool8 = pr2.NewPool(1024, func() *[]byte {
+var bytesPool8 = pr2.NewPool(1024, func(_ pr2.PoolPutFunc) *[]byte {
 	bs := make([]byte, 8)
 	return &bs
 })
 
 // 32K * 32 = 1M
-var bytesPool32K = pr2.NewPool(32, func() *[]byte {
+var bytesPool32K = pr2.NewPool(32, func(_ pr2.PoolPutFunc) *[]byte {
 	bs := make([]byte, 32*1024)
 	return &bs
 })
