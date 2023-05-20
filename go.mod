36fc1489b37a6c912d5f06c25e731dd6b62d4614
reus <reusee@gmail.com>
2023-05-20 09:42:46 +0800

    update pr2

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.18
 
 require (
 	github.com/reusee/e5 v0.0.0-20230128094953-f2ff5c9c135a
-	github.com/reusee/pr2 v0.0.0-20230513031402-df695c66b2a2
+	github.com/reusee/pr2 v0.0.0-20230520013926-e3502630520e
 	golang.org/x/exp v0.0.0-20230321023759-10a507213a29
 )
--- pool.go
+++ pool.go
@@ -3,13 +3,13 @@ package sb
 import "github.com/reusee/pr2"
 
 // 8 * 1024 = 8K
-var bytesPool8 = pr2.NewPool(1024, func(_ pr2.PoolPutFunc) *[]byte {
+var bytesPool8 = pr2.NewPool(1024, func() *[]byte {
 	bs := make([]byte, 8)
 	return &bs
-}, pr2.ResetSlice[byte](8, -1))
+}).WithReset(pr2.ResetSlice[byte](8, -1))
 
 // 32K * 32 = 1M
-var bytesPool32K = pr2.NewPool(32, func(_ pr2.PoolPutFunc) *[]byte {
+var bytesPool32K = pr2.NewPool(32, func() *[]byte {
 	bs := make([]byte, 32*1024)
 	return &bs
-}, pr2.ResetSlice[byte](32*1024, -1))
+}).WithReset(pr2.ResetSlice[byte](32*1024, -1))
